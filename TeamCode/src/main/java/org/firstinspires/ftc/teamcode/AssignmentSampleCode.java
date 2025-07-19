47c7a2355032e912ccca2a7f4507557c03274419
osqwety <lamborgini8032@gmail.com>
2025-07-19 12:47:08 -0500

    fixed code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AssignmentSampleCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AssignmentSampleCode.java
@@ -4,12 +4,15 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @Autonomous(name = "AssignmentSampleCode", group = "Robot")
 public class AssignmentSampleCode extends LinearOpMode {
 
-    public DcMotorEx backRight;
-    public DcMotorEx backLeft;
+    public DcMotorEx backRight = null;
+    public DcMotorEx backLeft = null;
+    public DcMotorEx frontLeft = null;
+    public DcMotorEx frontRight = null;
 
     //Ticks is motor rate * the ratio provided by Gobilda.
     public final double ticks = 537.7;
@@ -17,11 +20,20 @@ public class AssignmentSampleCode extends LinearOpMode {
     public final double circumference = 301.59;
     @Override
     public void runOpMode() {
-        backRight = hardwareMap.get(DcMotorEx.class, "right_front_drive");
-        backLeft = hardwareMap.get(DcMotorEx.class, "left_front_drive");
+        backRight = hardwareMap.get(DcMotorEx.class, "right_back_drive");
+        backLeft = hardwareMap.get(DcMotorEx.class, "left_back_drive");
+        frontLeft = hardwareMap.get(DcMotorEx.class, "left_front_drive");
+        frontRight = hardwareMap.get(DcMotorEx.class, "right_front_drive");
 
+        frontRight.setDirection(DcMotor.Direction.FORWARD);
+        frontLeft.setDirection(DcMotor.Direction.REVERSE);
+        backRight.setDirection(DcMotor.Direction.FORWARD);
         backRight.setDirection(DcMotor.Direction.REVERSE);
-        backLeft.setDirection(DcMotor.Direction.FORWARD);
+
+        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         waitForStart();
 
